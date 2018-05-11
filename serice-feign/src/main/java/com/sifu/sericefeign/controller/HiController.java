e0f5357814b055c5363b7d6ef43d49b1f0ae4f6f
sifu <2587107273@qq.com>
2018-05-12 00:11:01 +0800

    第三章

---
+++ serice-feign/src/main/java/com/sifu/sericefeign/controller/HiController.java
@@ -0,0 +1,23 @@
+package com.sifu.sericefeign.controller;
+
+import com.sifu.sericefeign.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sifu
+ * @version 1.0
+ * @date 2018/5/12
+ */
+@RestController
+public class HiController {
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
---
+++ serice-feign/src/main/java/com/sifu/sericefeign/service/SchedualServiceHi.java
@@ -0,0 +1,17 @@
+package com.sifu.sericefeign.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author sifu
+ * @version 1.0
+ * @date 2018/5/12
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}

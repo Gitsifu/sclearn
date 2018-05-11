45f7c3acac79a952f1f24ccede4483852b98b0e8
sifu <2587107273@qq.com>
2018-05-11 23:20:47 +0800

    第一章

---
+++ src/main/java/com/sifu/sclearn/SclearnApplication.java
@@ -0,0 +1,12 @@
+package com.sifu.sclearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SclearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SclearnApplication.class, args);
+    }
+}

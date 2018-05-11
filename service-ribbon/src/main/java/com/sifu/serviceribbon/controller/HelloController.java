49fff3f24e02bc2d7c77309896db725a6fe49fd3
sifu <2587107273@qq.com>
2018-05-11 23:36:44 +0800

    第二章

---
+++ service-ribbon/src/main/java/com/sifu/serviceribbon/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.sifu.serviceribbon.controller;
+
+import com.sifu.serviceribbon.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author sifu
+ * @version 1.0
+ * @date 2018/5/11
+ */
+@RestController
+public class HelloController {
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService(name);
+    }
+
+}
